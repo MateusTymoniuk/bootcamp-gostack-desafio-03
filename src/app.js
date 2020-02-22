831e63ad47e582e3d5e6d11b4bc8b8efa719a770
Mateus Tymoniuk <tymoniuk.m@gmail.com>
2020-02-22 19:09:22 -0300

    Adicionando sentry e middleware de erros

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 import express from 'express';
+import * as Sentry from '@sentry/node';
 import path from 'path';
+import sentryConfig from './config/sentry';
+import 'express-async-errors';
 import routes from './routes';
 
 import './database';
+import errorMiddleware from './app/middlewares/error';
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -21,6 +28,11 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(errorMiddleware);
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://e18ebccb6cbb495e8325b17e863c5002@sentry.io/2774447',
+};
